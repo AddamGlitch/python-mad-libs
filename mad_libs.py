ec50c6f70826473831a3e4ecf5cd73cd943deb2b
AddamGlitch <48646877+AddamGlitch@users.noreply.github.com>
2021-01-15 20:54:50 +0000

    Add files via upload

---
+++ mad_libs.py
@@ -0,0 +1,19 @@
+print('Please Enter the following:')
+print()
+adjective = input('adjective: ')
+animal = input('animal: ')
+verb1 = input('verb: ')
+exclamation = input('exclamation: ')
+verb2 = input('verb: ')
+verb3 = input('verb: ')
+print()
+print('Your story is:')
+print()
+print(
+'The other day, I was really in trouble. It all started when I saw a\n\
+very '+ adjective +' '+ animal.capitalize() +' '+ verb1 +' down the hallway.\n\
+"'+ exclamation.capitalize() + '!"'' I yelled. But all I could think to do was to\n\
+'+ verb2 +' over and over. Miraculously, that caused it to stop, but\n\
+not before it tried to '+ verb3 +' right in front of my family.'
+)
+print()
